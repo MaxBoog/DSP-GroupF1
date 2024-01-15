154d75efc579eb1806e889883e25274a93ba3d96
Max Boog <maxboog@sp-byods-145-109-8-51.wireless.uva.nl>
2024-01-15 11:49:42 +0100

    stardog backend

--- datagem/src/app/(pages)/dashboard/page.js
+++ datagem/src/app/(pages)/dashboard/page.js
@@ -1,7 +1,24 @@
-export default function Page() {
+async function getData() {
+  const res = await fetch(
+    "https://sd-58d1da07.stardog.cloud:5820/admin/data_sources/tutorial-database/available"
+  );
+  // The return value is *not* serialized
+  // You can return Date, Map, Set, etc.
+
+  if (!res.ok) {
+    // This will activate the closest `error.js` Error Boundary
+    throw new Error("Failed to fetch data");
+  }
+
+  return res.json();
+}
+
+export default async function Page() {
+  const data = await getData();
+
   return (
     <>
-      <h1>Dashboard</h1>
+      <h1></h1>
     </>
   );
 }
