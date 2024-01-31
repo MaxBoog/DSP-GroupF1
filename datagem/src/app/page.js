43c8e0b98b6d731ebdf8e01cad9ad9c78f12ba3e
Max Boog <maxboog@MacBook-Air-van-Max.local>
2024-01-31 18:36:50 +0100

    home page redirect to signup

--- datagem/src/app/page.js
+++ datagem/src/app/page.js
@@ -50,7 +50,7 @@ export default function Home() {
               </p>
               <div className="mt-10 flex items-center justify-center gap-x-6">
                 <Link
-                  href="/signup/1"
+                  href="/signup"
                   className="rounded-md bg-indigo-700 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                 >
                   Get started
